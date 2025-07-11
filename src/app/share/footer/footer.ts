80f3cb8497579a99a6fe2b470729f5b1da4c99a4
jamesLaurino <petersthomas90@gmail.com>
2025-07-11 17:24:47 +0200

    FTA-088 landing page and setup

--- src/app/share/footer/footer.ts
+++ src/app/share/footer/footer.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './footer.html',
   styles: ''
 })
 export class Footer {
-
+  getDate() {
+     return new Date();
+   }
 }
