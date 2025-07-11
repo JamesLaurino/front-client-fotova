467e1ceb40afa64745ca673fffb904443c040449
jamesLaurino <petersthomas90@gmail.com>
2025-07-11 16:54:55 +0200

    FTA-088 landing page and setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ProductList} from './product/product-list/product-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'products', component: ProductList}
+];
---
+++ src/app/share/nav-bar/nav-bar.ts
@@ -0,0 +1,18 @@
+import {Component, inject} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [],
+  templateUrl: './nav-bar.html',
+  styles: ''
+})
+export class NavBar {
+
+  readonly #router = inject(Router)
+
+
+  goToAllProducts() {
+    this.#router.navigate(['/products']);
+  }
+}
