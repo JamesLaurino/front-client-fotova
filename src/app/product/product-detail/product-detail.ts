471cead0069ea2c90c4c0abdc7a60a8d6a68d75c
jamesLaurino <petersthomas90@gmail.com>
2025-07-15 17:18:13 +0200

    FTA-090 Display one product

--- src/app/product/product-detail/product-detail.ts
+++ src/app/product/product-detail/product-detail.ts
@@ -1,15 +1,17 @@
-import {Component, inject} from '@angular/core';
+import {Component, ElementRef, inject, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProductService} from '../../service/interfaces/product-service';
 import {rxResource} from '@angular/core/rxjs-interop';
 import {map, tap} from 'rxjs';
 import urlHelper from '../../helper/url-helper';
 import {CurrencyPipe} from '@angular/common';
+import {ProductBorderDirective} from '../../share/directives/product-border-directive';
 
 @Component({
   selector: 'app-product-detail',
   imports: [
-    CurrencyPipe
+    CurrencyPipe,
+    ProductBorderDirective
   ],
   templateUrl: './product-detail.html',
   styles: ''
@@ -22,6 +24,8 @@ export class ProductDetail {
   private productId = this.#route.snapshot.params['id'];
   protected readonly urlHelper = urlHelper;
 
+  @ViewChild('imgSwitch') imgSwitch!: ElementRef;
+
   product = rxResource({
     stream: () => {
       return this.#productService.getProductById(this.productId)
@@ -31,4 +35,8 @@ export class ProductDetail {
         )
     }
   })
+
+  switchImage(img: string) {
+    this.imgSwitch.nativeElement.src = this.urlHelper(img);
+  }
 }
