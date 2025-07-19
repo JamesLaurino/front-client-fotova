918bebf2eb1c597a051e7de75618b81b5ce51eef
jamesLaurino <petersthomas90@gmail.com>
2025-07-19 18:41:14 +0200

    FTA-091 create cart shopping

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NavBar} from './share/navbar/nav-bar';
-import {Footer} from './share/footer/footer';
+import {CartService} from './service/interfaces/cart-service';
+import {CartHelper} from './helper/cart-helper';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBar, RouterOutlet, Footer],
+  imports: [NavBar, RouterOutlet],
   templateUrl: './app.html',
   styles: ''
 })
 export class App {
-  protected title = 'front-client-fotova';
+
+  readonly #cartService = inject(CartService)
+  readonly #cartHelper = inject(CartHelper)
+
+  constructor() {
+    this.#cartService.getCarts().subscribe(
+      (carts) => {
+        this.#cartHelper.cartsQuantity.update((n:number) => n = 0);
+        for (const item of carts) {
+          this.#cartHelper.cartsQuantity.update((n:number) => n += item.quantity)
+        }
+      }
+    )
+  }
 }
