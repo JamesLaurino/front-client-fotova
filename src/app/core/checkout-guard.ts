db1b8560207b710fe3c1ad838c975cf78344d0ff
jamesLaurino <petersthomas90@gmail.com>
2025-07-22 12:17:01 +0200

    refactoring

--- src/app/core/checkout-guard.ts
+++ src/app/core/checkout-guard.ts
@@ -1,15 +1,17 @@
-import {CanActivateFn} from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {UserService} from '../service/user/user-service';
-import {map} from 'rxjs';
+import {catchError, map, of} from 'rxjs';
 
 export const CheckoutGuard: CanActivateFn = () => {
   const userService = inject(UserService);
+  const router = inject(Router);
 
   return userService.getUserInformation().pipe(
     map(user => {
-      console.log(user);
-      return user.data.ClientAddressDto !== null;
-    })
+      console.log(user)
+      return user.data.ClientAddressDto !== null ? true : router.createUrlTree(['/']);
+    }),
+    catchError(() => of(router.createUrlTree(['/login'])))
   );
 };
