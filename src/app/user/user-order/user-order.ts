b9b1665e6d0723095530b8d739ec9d1314f917a1
jamesLaurino <petersthomas90@gmail.com>
2025-07-22 15:49:13 +0200

    FTA-095 Create panel user

--- src/app/user/user-order/user-order.ts
+++ src/app/user/user-order/user-order.ts
@@ -1,15 +1,30 @@
-import {Component, input, InputSignal} from '@angular/core';
+import {Component, computed, input, InputSignal, signal} from '@angular/core';
 import {OrderModel} from '../../model/order/Order-model';
-import {JsonPipe} from '@angular/common';
+import {CurrencyPipe, DatePipe, JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-user-order',
   imports: [
-    JsonPipe
+    JsonPipe,
+    DatePipe,
+    CurrencyPipe,
   ],
   templateUrl: './user-order.html',
   styleUrl: './user-order.css'
 })
 export class UserOrder {
   userOrdersInput:InputSignal<OrderModel[] | undefined> = input.required<OrderModel[] | undefined>();
+
+  ordersOrdered = computed(() => {
+    let myMap = new Map<number, OrderModel[]>();
+    this.userOrdersInput()?.forEach(order => {
+        if (!myMap.has(order.orderId)) {
+          myMap.set(order.orderId, [order]);
+        } else {
+          myMap.get(order.orderId)!.push(order);
+        }
+    });
+    return Array.from(myMap.values());
+  })
+
 }
