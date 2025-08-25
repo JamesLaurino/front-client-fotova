3f73a6d3053fc3e5d2d196d96b7aa2a0ad4693cc
jamesLaurino <petersthomas90@gmail.com>
2025-08-25 15:03:47 +0200

    refactoring

--- src/app/helper/url-helper.ts
+++ src/app/helper/url-helper.ts
@@ -3,8 +3,7 @@ import {environment} from '../../environments/environment';
 export default function urlHelper(url:string):string {
   let baseUrl = environment.baseUrl;
   if(url.startsWith('file:')) {
-    let urlSplit = url.split('/')[12] + '/' + url.split('/')[13];
-    return baseUrl + "/" + urlSplit;
+    return baseUrl + "/images/" + url.split('/').at(-1);
   } else {
     return baseUrl + "/images/" + url;
   }
