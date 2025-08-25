40d665c59ff75ccf5edf74389782f61a3687e53f
jamesLaurino <petersthomas90@gmail.com>
2025-08-25 15:03:35 +0200

    FTA-114 create admin panel chart

---
+++ src/app/admin/admin-chart/admin-chart.ts
@@ -0,0 +1,68 @@
+import {Component, effect, inject, input, InputSignal} from '@angular/core';
+import {Router} from '@angular/router';
+import {I18nService} from '../../service/i18n/i18nService';
+import {OrderDetail} from '../../model/order/order-detail';
+import {ChartConfiguration, ChartData, ChartType} from 'chart.js';
+import {BaseChartDirective} from "ng2-charts";
+import 'chart.js/auto';
+
+@Component({
+  selector: 'app-admin-chart',
+  imports: [BaseChartDirective],
+  templateUrl: './admin-chart.html',
+  styleUrl: './admin-chart.css'
+})
+export class AdminChart {
+  ordersDetailedInput: InputSignal<OrderDetail[] | undefined> = input.required<OrderDetail[] | undefined>();
+  readonly #router = inject(Router);
+  readonly i18n = inject(I18nService);
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    scales: {
+      x: {},
+      y: {
+        min: 0
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      {data: [], label: 'Monthly Sales'}
+    ]
+  };
+
+  constructor() {
+    effect(() => {
+      const orders = this.ordersDetailedInput();
+      if (orders) {
+        this.processOrders(orders);
+      }
+    });
+  }
+
+  private processOrders(orders: OrderDetail[]) {
+    const monthlySales = new Map<string, number>();
+    orders.forEach(order => {
+      const month = new Date(order.creationDate).toLocaleString('default', {month: 'long'});
+      const currentSales = monthlySales.get(month) || 0;
+      monthlySales.set(month, currentSales + order.total);
+    });
+
+    const sortedMonths = Array.from(monthlySales.keys()).sort((a, b) => {
+        const aDate = new Date(`01 ${a} 2000`);
+        const bDate = new Date(`01 ${b} 2000`);
+        return aDate.getMonth() - bDate.getMonth();
+    });
+
+    this.barChartData.labels = sortedMonths;
+    this.barChartData.datasets[0].data = sortedMonths.map(month => monthlySales.get(month) || 0);
+  }
+}
