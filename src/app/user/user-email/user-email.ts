61edc648b4dc7e40f7ac8e3ce7a2ea30ecf9c9c1
jamesLaurino <petersthomas90@gmail.com>
2026-01-11 14:07:59 +0100

    FTA-115 implement user email

--- src/app/user/user-email/user-email.ts
+++ src/app/user/user-email/user-email.ts
@@ -1,7 +1,10 @@
-import { Component, InputSignal, input, inject } from '@angular/core';
+import {Component, InputSignal, input, inject, Output, EventEmitter} from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {I18nService} from '../../service/i18n/i18nService';
 import {ClientEmail} from '../../model/client/client-email';
+import {UserService} from '../../service/user/user-service';
+import {ToasterService} from '../../service/toaster/toasterService';
+import {ClientResponseEmailApi} from '../../model/client/client-response-email-api';
 
 
 @Component({
@@ -15,6 +18,10 @@ import {ClientEmail} from '../../model/client/client-email';
 export class UserEmail {
 
   userEmailInput: InputSignal<string | undefined> = input.required<string | undefined>();
+  @Output() emailUpdated = new EventEmitter<void>();
+
+  readonly #userService = inject(UserService)
+  private toasterService = inject(ToasterService);
   readonly i18n = inject(I18nService);
   private fb = inject(FormBuilder);
 
@@ -43,9 +50,35 @@ export class UserEmail {
       message: this.form.value.message!
     };
 
-    console.log('ClientEmail payload:', emailPayload);
-
-    // Exemple :
-    // this.emailService.sendEmail(emailPayload).subscribe(...)
+    // sending email to server
+    this.#userService.sendEmail(emailPayload).subscribe({
+      next: (response: ClientResponseEmailApi) => {
+        if (response.responseCode === 200) {
+          this.emailUpdated.emit();
+          this.toasterService.show({
+            toastTitle: 'Mail success',
+            toastTime: 'il y a 1 min',
+            toastImageUrl: '/fotova/check.jpg',
+            toastMessage: 'Email send with success'
+          })
+        } else {
+          this.toasterService.show({
+            toastTitle: 'Mail error',
+            toastTime: 'il y a 1 min',
+            toastImageUrl: '/fotova/error.png',
+            toastMessage: 'Une erreur est survenue lors de l\'envoie du mail veuillez tenter plus tard'
+          })
+        }
+      },
+      error: (error: any) => {
+        const message = error.error.errorList[0]
+        this.toasterService.show({
+          toastTitle: 'Mail error',
+          toastTime: 'il y a 1 min',
+          toastImageUrl: '/fotova/error.png',
+          toastMessage: message
+        })
+      }
+    });
   }
 }
