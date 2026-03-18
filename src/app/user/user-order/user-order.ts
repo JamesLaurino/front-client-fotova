0ad05d9fb62af67e962de6dfa19e4f100bd0417f
jamesLaurino <petersthomas90@gmail.com>
2026-03-18 23:13:59 +0100

    FTO-153 improving order page user with i18

--- src/app/user/user-order/user-order.ts
+++ src/app/user/user-order/user-order.ts
@@ -2,6 +2,9 @@ import {Component, computed, inject, input, InputSignal} from '@angular/core';
 import {OrderModel} from '../../model/order/Order-model';
 import {CurrencyPipe, DatePipe} from '@angular/common';
 import {I18nService} from '../../service/i18n/i18nService';
+import {rxResource} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
+import {LabelService} from '../../service/label/label-service';
 
 @Component({
   selector: 'app-user-order',
@@ -14,13 +17,30 @@ import {I18nService} from '../../service/i18n/i18nService';
 })
 export class UserOrder {
   userOrdersInput:InputSignal<OrderModel[] | undefined> = input.required<OrderModel[] | undefined>();
+  selectedOrder: any[] | null = null;
+  readonly #labelService = inject(LabelService);
   readonly i18n = inject(I18nService);
 
   getOrderTotal(order: any[]): number {
     return order.reduce((acc, item) => acc + (item.price * item.quantity), 0);
   }
 
-  selectedOrder: any[] | null = null;
+  labels = rxResource({
+    stream: () => {
+      return this.#labelService.getAllLabels()
+        .pipe(
+          map(response => response.data)
+        )
+    }
+  })
+
+  getLabelForOrder(productName: string) {
+    const labels = this.labels.value();
+    if (!labels) return undefined;
+
+    return labels.find(label =>
+      label.titleEn === productName || label.titleRu === productName ||label.titleFr === productName)
+  }
 
   openOrder(order: any[]) {
     this.selectedOrder = order;
