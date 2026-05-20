3b14ecddd9262923adfb8cbc19523272de412351
jamesLaurino <petersthomas90@gmail.com>
2026-05-20 20:48:15 +0200

    FTO-183 admin-client

--- src/app/admin/admin-client/admin-client.ts
+++ src/app/admin/admin-client/admin-client.ts
@@ -1,4 +1,4 @@
-import {Component, inject, input, InputSignal} from '@angular/core';
+import {Component, computed, inject, input, InputSignal} from '@angular/core';
 import {I18nService} from '../../service/i18n/i18nService';
 import {ClientAll} from '../../model/client/client-all';
 import {Router} from '@angular/router';
@@ -10,9 +10,13 @@ import {Router} from '@angular/router';
   styleUrls: ['admin-client.css'],
 })
 export class AdminClient {
-  clientsInput:InputSignal<ClientAll[] | undefined> = input.required<ClientAll[] | undefined>();
+  clientsInput: InputSignal<ClientAll[] | undefined> = input.required<ClientAll[] | undefined>();
   readonly #router = inject(Router);
   readonly i18n = inject(I18nService);
+
+  readonly activeCount = computed(() =>
+    this.clientsInput()?.filter(c => c.isActive).length ?? 0
+  );
 
   goToClientDetail(id: number) {
     this.#router.navigate(['/admin/client', id]);
